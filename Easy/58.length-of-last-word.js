6f03b6aa7dc46714f0cb0a8be637be38790a006a
luanwin <luanxuechaowd@gmail.com>
2019-03-20 20:07:36 +0800

    daily commit

---
+++ Easy/58.length-of-last-word.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=58 lang=javascript
+ *
+ * [58] Length of Last Word
+ *
+ * https://leetcode.com/problems/length-of-last-word/description/
+ *
+ * algorithms
+ * Easy (32.14%)
+ * Total Accepted:    251.2K
+ * Total Submissions: 780.7K
+ * Testcase Example:  '"Hello World"'
+ *
+ * Given a string s consists of upper/lower-case alphabets and empty space
+ * characters ' ', return the length of last word in the string.
+ * 
+ * If the last word does not exist, return 0.
+ * 
+ * Note: A word is defined as a character sequence consists of non-space
+ * characters only.
+ * 
+ * Example:
+ * 
+ * Input: "Hello World"
+ * Output: 5
+ * 
+ * 
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    if(!s || !s.trim())return 0
+    let arr = s.replace(/ /g,',').split(',');
+    let list = arr.filter((s)=>{
+        return s && s.trim(); 
+    })
+    return list[list.length-1].length;
+};
+
