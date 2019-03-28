9075ebcf7020d8151c45a0841d178fe03cf5be0d
luanwin <luanxuechaowd@gmail.com>
2019-03-28 20:03:11 +0800

    daily commmit

---
+++ Easy/100.same-tree.js
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=100 lang=javascript
+ *
+ * [100] Same Tree
+ *
+ * https://leetcode.com/problems/same-tree/description/
+ *
+ * algorithms
+ * Easy (49.59%)
+ * Total Accepted:    358.4K
+ * Total Submissions: 722.4K
+ * Testcase Example:  '[1,2,3]\n[1,2,3]'
+ *
+ * Given two binary trees, write a function to check if they are the same or
+ * not.
+ * 
+ * Two binary trees are considered the same if they are structurally identical
+ * and the nodes have the same value.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:     1         1
+ * ⁠         / \       / \
+ * ⁠        2   3     2   3
+ * 
+ * ⁠       [1,2,3],   [1,2,3]
+ * 
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:     1         1
+ * ⁠         /           \
+ * ⁠        2             2
+ * 
+ * ⁠       [1,2],     [1,null,2]
+ * 
+ * Output: false
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input:     1         1
+ * ⁠         / \       / \
+ * ⁠        2   1     1   2
+ * 
+ * ⁠       [1,2,1],   [1,1,2]
+ * 
+ * Output: false
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    if (p == null || q == null) return p == q;
+    return p.val == q.val && isSameTree(p.left, q.left)
+    && isSameTree(p.right, q.right);
+};
+
