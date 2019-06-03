4adc527ea1ca9256708ee22d1b062da57e10c635
luanxuechao <luanxuechaowd@gmail.com>
2019-06-03 13:37:40 +0800

    daily commit

---
+++ Easy/283.move-zeroes.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=283 lang=javascript
+ *
+ * [283] Move Zeroes
+ */
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let len =nums.length;
+    for(let i =0;i<len;i++){
+        if(nums[i]===0){
+            nums.splice(i,1);
+            nums.push(0);
+            --i;
+            len--;
+        }
+    }
+    
+};
+
